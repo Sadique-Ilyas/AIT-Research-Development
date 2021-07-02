d3e8fc41f51974991472d9f6bb349973d53e28eb
Sadique Ilyas <sadiqueilyasfifa@gmail.com>
2021-07-03 00:33:21 +0530

    Complete !

---
+++ app/src/main/java/com/example/aitresearchdevelopment/Upload.java
@@ -0,0 +1,34 @@
+package com.example.aitresearchdevelopment;
+
+public class Upload {
+
+    String time_difference;
+    long upTime;
+
+    public Upload() {
+    }
+
+    public Upload(String time_difference) {
+        this.time_difference = time_difference;
+    }
+
+    public Upload(long upTime) {
+        this.upTime = upTime;
+    }
+
+    public String getTime_difference() {
+        return time_difference;
+    }
+
+    public void setTime_difference(String time_difference) {
+        this.time_difference = time_difference;
+    }
+
+    public long getUpTime() {
+        return upTime;
+    }
+
+    public void setUpTime(long upTime) {
+        this.upTime = upTime;
+    }
+}
